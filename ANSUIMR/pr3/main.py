a4679b1e9ea7d6448fcd0a2c6d02a8b8c9c8f63a
Sekou <sekoudiane1990@gmail.com>
2025-09-17 19:48:39 +0300

    Add files via upload

---
+++ ANSUIMR/pr3/main.py
@@ -0,0 +1,77 @@
+
+import sys, pygame
+import numpy as np
+import math
+
+class Ngon: #многоугольник
+    def __init__(self):
+        self.pts=[]
+    def add_pt(self, p):
+        self.pts.append(p)
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (0,0,255), self.pts, 2)
+
+pygame.font.init()
+def draw_text(screen, s, x, y, sz=15, с=(0, 0, 0)):  # отрисовка текста
+    screen.blit(pygame.font.SysFont('Comic Sans MS', sz).render(s, True, с), (x, y))
+
+def rot(v, ang): #поворот вектора на угол
+    s, c = math.sin(ang), math.cos(ang)
+    return [v[0] * c - v[1] * s, v[0] * s + v[1] * c]
+
+def lim_ang(ang): #ограничение угла в пределах +/-pi
+    while ang > math.pi: ang -= 2 * math.pi
+    while ang <= -math.pi: ang += 2 * math.pi
+    return ang
+
+def rot_arr(vv, ang): # функция для поворота массива на угол
+    return [rot(v, ang) for v in vv]
+
+def dist(p1, p2): #расстояние между точками
+    return np.linalg.norm(np.subtract(p1, p2))
+
+def draw_rot_rect(screen, color, pc, w, h, ang): #точка центра, ширина высота прямоуг и угол поворота прямогуольника
+    pts = [
+        [- w/2, - h/2],
+        [+ w/2, - h/2],
+        [+ w/2, + h/2],
+        [- w/2, + h/2],
+    ]
+    pts = rot_arr(pts, ang)
+    pts = np.add(pts, pc)
+    pygame.draw.polygon(screen, color, pts, 2)
+
+sz = (800, 600)
+
+def main():
+    screen = pygame.display.set_mode(sz)
+    pygame.display.set_caption('Animation 2D')
+    timer = pygame.time.Clock()
+    fps = 20
+    dt=1/fps
+
+    ngon=Ngon()
+    ngon.add_pt((200,200))
+    ngon.add_pt((300, 400))
+    ngon.add_pt((400, 200))
+
+    while True:
+        for ev in pygame.event.get():
+            if ev.type==pygame.QUIT:
+                sys.exit(0)
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_r:
+                    print("Hi")
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                ngon.add_pt(ev.pos)
+
+        screen.fill((255, 255, 255))
+        ngon.draw(screen)
+        draw_text(screen, f"Test = {1}", 5, 5)
+
+        pygame.display.flip()
+        timer.tick(fps)
+
+main()
+
+#template file by S. Diane, RTU MIREA, 2024
