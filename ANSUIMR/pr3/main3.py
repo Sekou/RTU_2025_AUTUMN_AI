67d6ed1f12c1f2eaa83bffb85a0cb28ec2309398
Sekou <sekoudiane1990@gmail.com>
2025-09-24 18:58:36 +0300

    Add files via upload

--- ANSUIMR/pr3/main3.py
+++ ANSUIMR/pr3/main3.py
@@ -14,12 +14,15 @@ def tr(pt): #transform pt from m to px
 def tr_(pt): #transform pt from px to m
     return (pt[0]/SCALE+SHIFT[0], pt[1]/SCALE+SHIFT[1])
 
+def snap(pt, delta=0.1):
+    return (round(pt[0], 1), round(pt[1], 1))
+
 class Ngon: #многоугольник
     def __init__(self):
         self.pts=[]
         self.closed=False
         self.tmp_pt=None
-        self.info=""
+        self.info, self.info2="", ""
     def add_pt_px(self, p_px):
         self.pts.append(tr_(p_px))
     def clear(self): self.pts=[]
@@ -31,19 +34,28 @@ class Ngon: #многоугольник
             d = json.loads(f.read())
             self.pts=d["pts"]
             self.closed=d["closed"]
-
+    def get_perimeter(self):
+        return sum(dist(p1, p2) for p1, p2 in zip(self.pts[1:], self.pts[:1]))
+    def get_area(self):
+        # определяем площадь многоугольника
+        x, y = [p[0] for p in self.pts], [p[1] for p in self.pts]  # get x and y in vectors
+        x_, y_ = x - np.mean(x), y - np.mean(y)  # shift coordinates
+        main_area = np.dot(x_[:-1], y_[1:]) - np.dot(y_[:-1], x_[1:])  # calculate area
+        correction = x_[-1] * y_[0] - y_[-1] * x_[0]
+        return 0.5 * np.abs(main_area + correction)
     def draw(self, screen):
-
         if len(self.pts)>1:
             pp=[tr(p) for p in self.pts]
             pygame.draw.lines(screen, (0,0,255), self.closed, pp, 2)
         if self.tmp_pt and len(self.pts)>0:
             pygame.draw.circle(screen, (0,255,255), tr(self.tmp_pt), 5, 2)
             p1, p2 = tr(self.pts[-1]), tr(self.tmp_pt)
-            pygame.draw.line(screen, (0,255,255), p1, p2, 2)
+            if dist(self.pts[-1], self.pts[0])>0.1:
+                pygame.draw.line(screen, (0,255,255), p1, p2, 2)
             L=dist(self.pts[-1], self.tmp_pt)
             x, y=self.tmp_pt
             self.info = f"End point = ({x:.2f}, {y:.2f}), end segm. len. = {L:.2f}"
+            self.info2 = f"Perimeter = {self.get_perimeter():.2f}, area = {self.get_area():.2f}"
         for p in self.pts:
             pygame.draw.circle(screen, (0,0,255), tr(p), 5, 2)
 
@@ -102,23 +114,24 @@ def main():
                     ngon.load("ngon.txt")
             if ev.type == pygame.MOUSEBUTTONDOWN:
                 all_keys = pygame.key.get_pressed()
-                p = ev.pos
+                pos = tr(snap(tr_(ev.pos)))
+                p=pos
                 if all_keys[pygame.K_LCTRL] and p_last:
-                    p1 = [ev.pos[0], p_last[1]] #вертикаль
-                    p2 = [p_last[0], ev.pos[1]] #горизонталь
-                    d1, d2 = dist(ev.pos, p1), dist(ev.pos, p2)
+                    p1 = [pos[0], p_last[1]] #вертикаль
+                    p2 = [p_last[0], pos[1]] #горизонталь
+                    d1, d2 = dist(pos, p1), dist(pos, p2)
                     if min(d1, d2)<50: p = p1 if d1<d2 else p2 #свободный наклон
                 ngon.add_pt_px(p)
                 p_last=p
             if ev.type == pygame.MOUSEMOTION:
-                ngon.tmp_pt=tr_(ev.pos)
-
-
+                pos = tr(snap(tr_(ev.pos)))
+                ngon.tmp_pt=tr_(pos)
 
         screen.fill((255, 255, 255))
         ngon.draw(screen)
         draw_text(screen, f"SCALE = {SCALE}, SHIFT = {SHIFT}", 5, 5)
-        draw_text(screen, f"Info = {ngon.info}", 5, 25)
+        draw_text(screen, f"{ngon.info}", 5, 25)
+        draw_text(screen, f"{ngon.info2}", 5, 45)
 
         pygame.display.flip()
         timer.tick(fps)
